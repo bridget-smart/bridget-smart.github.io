11b5cce0f8f137fc731069fb6c8bad7da379aa1f
bridget-smart <bridget.anna.smart@gmail.com>
2024-09-16 12:12:17 +0100

    New Version Sept 16

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function() { stickyHeader() };
+
+let header = document.querySelector("header");
+let sticky = header.offsetTop;
+
+function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
